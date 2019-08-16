be795bc121ed14747e21b1ea64d0739ec0d20845
mcdomx <mcdomx@me.com>
2019-08-15 21:36:10 -0400

    updates ...

--- service_server/run_service_server.sh
+++ service_server/run_service_server.sh
@@ -7,12 +7,11 @@ echo "ROS_HOSTNAME: $ROS_HOSTNAME"
 echo "ROS_IP: $ROS_IP"
 echo "********************************************************"
 
-# Give time to startup
-sleep 2
 
 while ! (rosnode list | grep rosout); do echo "waiting..."; sleep 5 ; done
 
-python /catkin_ws/src/master_pkg/src/service_server.py
-# rosrun master_pkg service_server.py
+
+# python /catkin_ws/src/master_pkg/src/service_server.py
+rosrun master_pkg service_server.py
 
 echo ">>>>>>>>>>>> service server is running on master: $ROS_MASTER_URI"
