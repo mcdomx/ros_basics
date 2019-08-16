7d5da399b43a011085ec56a953a3f600f9600247
mcdomx <mcdomx@me.com>
2019-08-15 21:57:30 -0400

    updates ...

--- service_server/run_service_server.sh
+++ service_server/run_service_server.sh
@@ -12,7 +12,7 @@ while ! (rosnode list | grep rosout); do echo "waiting..."; sleep 5 ; done
 
 source ~/.bashrc
 
-# python /catkin_ws/src/master_pkg/src/service_server.py
-rosrun master_pkg service_server.py
+python /catkin_ws/src/master_pkg/src/service_server.py
+# rosrun master_pkg service_server.py
 
 echo ">>>>>>>>>>>> service server is running on master: $ROS_MASTER_URI"
