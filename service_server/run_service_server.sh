ff6fd5db0850b698eb1046ae715f8a32bd5c8562
mcdomx <mcdomx@me.com>
2019-08-15 22:07:51 -0400

    updates ...

--- service_server/run_service_server.sh
+++ service_server/run_service_server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "********************************************************"
 echo "SERVICE SERVER NODE"
@@ -8,14 +8,14 @@ echo "ROS_IP: $ROS_IP"
 echo "********************************************************"
 
 # Give time to startup
-sleep 3
+# sleep 3
 
 while ! (rosnode list | grep rosout); do echo "waiting..."; sleep 5 ; done
 
 # not sure if I need this - need to use bash to run /bin/bash
-source ~/.bashrc
+# source ~/.bashrc
 
-python /catkin_ws/src/master_pkg/src/service_server.py
+# python /catkin_ws/src/master_pkg/src/service_server.py
 # rosrun master_pkg service_server.py
 
 echo ">>>>>>>>>>>> service server is running on master: $ROS_MASTER_URI"
