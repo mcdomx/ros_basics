d4d434fefcc6b25f719255a2593344a5d0da15b5
mcdomx <mcdomx@me.com>
2019-08-20 08:31:45 -0400

    added webserver

--- webserver/run.sh
+++ webserver/run.sh
@@ -6,6 +6,6 @@ set -e
 
 export LD_LIBRARY_PATH="/mjpg-streamer/mjpg-streamer-experimental"
 
-./mjpg_streamer -i input_raspicam.so -o output_http.so -w ./www 
+./mjpg_streamer -i input_raspicam.so -o 'output_http.so -w ./www' 
 
 
