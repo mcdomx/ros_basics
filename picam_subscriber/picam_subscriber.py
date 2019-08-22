d9d6ab356fbd738d7f96ae42529e30f0087f8891
mcdomx <mcdomx@me.com>
2019-08-22 08:09:59 -0400

    created picam publishing

---
+++ picam_subscriber/picam_subscriber.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# This script will subscribe to all publihsing picam nodes.
+# Data will be stored locally and published via web server.
+
+import rospy
+import sys
+
+from std_msgs.msg import UInt8MultiArray
+
+
+# called when published
+def callback_receive_data(msg):
+    rospy.loginfo("Message received")
+    # save data somewhere so that the webserver can access it
+    # overwrite anything that was already there.
+
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('picam_subscriber_node')
+    rospy.loginfo("Picam Subscriber has been started")
+
+    # Create a subscriber
+    subsciber_pidev1 = rospy.Subscriber("/pidev1_images", UInt8MultiArray, callback=callback_receive_data, callback_args="pidev1")
+
+    # stops and waits here without advancing
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print('Stopping Picam Subscriber.....')
+        sys.exit(2)
+
+    sys.exit(0)
---
+++ picam_subscriber/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "********************************************************"
+echo "SUBSCRIBER NODE"
+echo "Waiting for roscore to start on $ROS_MASTER_URI"
+echo "ROS_HOSTNAME: $ROS_HOSTNAME"
+echo "ROS_IP: $ROS_IP"
+echo "********************************************************"
+
+# Give time to startup
+sleep 2
+
+while ! (rosnode list | grep rosout); do echo "waiting..."; sleep 5 ; done
+
+python /catkin_ws/src/master_pkg/src/subscriber.py
+# rosrun master_pkg subscriber.py
+
+echo ">>>>>>>>>>>> subscriber is running on master: $ROS_MASTER_URI"
