8cf848a689f68963d5cb41c7c9cb6cdaedc483a6
mcdomx <mcdomx@me.com>
2019-08-22 08:19:06 -0400

    created picam publishing

--- roscore/run.sh
+++ roscore/run.sh
@@ -5,6 +5,10 @@
 echo "********************************************************"
 echo "Starting roscore"
 echo "********************************************************"
+
+# Set environment to execute ROS commands
+source /catkin_ws/devel/setup.bash
+
 roscore
 echo ">>>>>>>>>>>> roscore has started"
 
