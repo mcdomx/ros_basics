6b7962cb5398e6d82117adeb469d27c56f1e12a3
mcdomx <mcdomx@me.com>
2019-08-22 21:26:57 -0400

    added picam subscriber

--- roscore/run.sh
+++ roscore/run.sh
@@ -9,7 +9,10 @@ echo "********************************************************"
 # Set environment to execute ROS commands
 source /catkin_ws/devel/setup.bash
 
+rosparam set /registry_host $(hostname)
+echo ">>>>>> /registry_host set to $(hostname)"
+
 roscore
-echo ">>>>>>>>>>>> roscore has started"
+echo ">>>>>> roscore has stopped"
 
 
