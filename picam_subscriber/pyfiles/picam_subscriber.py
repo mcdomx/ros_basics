090cc265c798ad480495d15b0d4c28f443489cd2
mcdomx <mcdomx@me.com>
2019-08-23 09:21:46 -0400

    added picam subscriber

--- picam_subscriber/pyfiles/picam_subscriber.py
+++ picam_subscriber/pyfiles/picam_subscriber.py
@@ -117,8 +117,8 @@ if __name__ == '__main__':
         # look for topics that are no longer being published
         activeSubscriptions = reg.Registration.get_active_subscriptions()
         cur_topics = rospy.get_published_topics()
-        rospy.loginfo("cur_topics[:][0:1]:")
-        rospy.loginfo(cur_topics[:][0:1])
+        rospy.loginfo("cur_topics[][0:1]:")
+        rospy.loginfo(cur_topics[][0:1])
         rospy.loginfo("activeSubscriptions:")
         rospy.loginfo(activeSubscriptions)
         for activeTopic in activeSubscriptions:
